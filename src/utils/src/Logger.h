f8a2ae5a51c7f8274b7b61780f942d59223a8a57
Prasana Ganesh <77466443+Prasana-sys@users.noreply.github.com>
2025-10-23 23:33:51 -0400

    compiler error

--- src/utils/src/Logger.h
+++ src/utils/src/Logger.h
@@ -1,6 +1,8 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
+#include <Arduino.h>
+
 void logSaveData(uint32_t timestamp_ms, float pHVal, float VpH, float dieTemp, float Vbatt);
 
 #endif // LOGGER_H
--- src/utils/src/SleepManager.h
+++ src/utils/src/SleepManager.h
@@ -1,6 +1,8 @@
 #ifndef SLEEPMANAGER_H
 #define SLEEPMANAGER_H
 
+#include <Arduino.h>
+
 void lightSleep(uint32_t sleepDuration);
 void deepSleep();
 
